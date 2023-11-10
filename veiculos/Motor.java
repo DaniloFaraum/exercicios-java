7369655e5aa14e90b984cee0c640f280ff79d370
Danilo Faraum <112991335+DaniloFaraum@users.noreply.github.com>
2023-11-09 22:54:27 -0300

    Add files via upload

---
+++ veiculos/Motor.java
@@ -0,0 +1,21 @@
+import sun.util.locale.provider.LocaleServiceProviderPool;
+
+import java.util.Scanner;
+
+public class Motor {
+    protected int quantidadePistao;
+    protected int potencia;
+
+    public Motor() {
+        quantidadePistao = 0;
+        potencia = 0;
+    }
+
+    public void setAll() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o(a) quantidade de pistões");
+        quantidadePistao = scanner.nextInt();
+        System.out.println("Insira o(a) potencia");
+        potencia = scanner.nextInt();
+    }
+}
---
+++ veiculos/Teste.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
+public class Teste {
+        public static void main(String[] args) {
+            Veiculo corsa = new Veiculo();
+            for (int i = 1; i <= 5; i++)
+                corsa.setAll();
+        }
+}
